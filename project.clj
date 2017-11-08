9e6c6f47841a0934480ada8e3c870ce5316aeee3
Mark Woodhall <mark.woodhall@gmail.com>
2017-11-08 15:21:21 +0000

    Bump version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject marge "0.5.0"
+(defproject marge "0.6.0"
   :description "A library to generate markdown using Clojure data structures, hiccup for markdown."
   :url "http://github.com/markwoodhall/marge"
   :license {:name "The MIT License"
